59de2008c23512fc57eb4ae5f0636dc5a4396944
parkbyunggyu <bkbspark0725@naver.com>
2018-11-21 12:55:14 +0900

    Add /GIT/pbg_report.sh

--- pbg_report.sh
+++ pbg_report.sh
@@ -444,9 +444,9 @@ EOFF
 		echo "Core         : "`cat /proc/cpuinfo | grep "processor" | sed 's/^.*://g' |head -n 1|awk '{printf "%.0f", $1 + 1}'` >> ./pbg_ser${TODAY}.log
 		echo "Memory       :" `free -m | head -n 2 | tail -n 1 | awk '{printf "%.0f", $2 / 1000}'`"GB" >> ./pbg_ser${TODAY}.log
 		UU=`top -d 1 | head -n 1 | awk -F ',' '{ num=1; while (index($num,"load")==0) num=num+1; print num }'`
-	        LDA=`top -b -d 1 -u $OUSER | head -n 1 | awk -F ',' {'print $'${UU}''} | sed 's/^  //g'| sed 's/load average://'|sed 's/ //'` 
+	        LDA=`top -b -d 1 | head -n 1 | awk -F ',' {'print $'${UU}''} | sed 's/^  //g'| sed 's/load average://'|sed 's/ //'` 
 		echo "load average : ""$LDA">> ./pbg_ser$TODAY.log
-		CPUIO=`top -b -d 1 -u $OUSER | head -n 3 | tail -n 1 | awk -F ',' {'print $5'} | sed 's/wa//'|sed 's/%//'|sed 's/ //'`
+		CPUIO=`top -b -d 1 | head -n 3 | tail -n 1 | awk -F ',' {'print $5'} | sed 's/wa//'|sed 's/%//'|sed 's/ //'`
 		echo "CPU utilization rate for Disk I/O :""$CPUIO""%" >> ./pbg_ser$TODAY.log
 		echo "" >> ./pbg_ser${TODAY}.log
 		shared_buffers=`"$SPATH"psql $OPT -t -c "show shared_buffers"`
@@ -488,9 +488,9 @@ EOFF
 		echo "Core         : "`cat /proc/cpuinfo | grep "processor" | sed 's/^.*://g' |head -n 1|awk '{printf "%.0f", $1 + 1}'` >> ./pbg_ser${TODAY}.log
 		echo "Memory       :" `free -m | head -n 2 | tail -n 1 | awk '{printf "%.0f", $2 / 1000}'`"GB" >> ./pbg_ser${TODAY}.log
 		UU=`top -d 1 | head -n 1 | awk -F ',' '{ num=1; while (index($num,"load")==0) num=num+1; print num }'`
-	        LDA=`top -b -d 1 -u $OUSER | head -n 1 | awk -F ',' {'print $'${UU}''} | sed 's/^  //g'| sed 's/load average://'|sed 's/ //'` 
+	        LDA=`top -b -d 1 | head -n 1 | awk -F ',' {'print $'${UU}''} | sed 's/^  //g'| sed 's/load average://'|sed 's/ //'` 
 		echo "load average : ""$LDA">> ./pbg_ser$TODAY.log
-		CPUIO=`top -b -d 1 -u $OUSER | head -n 3 | tail -n 1 | awk -F ',' {'print $5'} | sed 's/wa//'|sed 's/%//'|sed 's/ //'`
+		CPUIO=`top -b -d 1 | head -n 3 | tail -n 1 | awk -F ',' {'print $5'} | sed 's/wa//'|sed 's/%//'|sed 's/ //'`
 		echo "CPU utilization rate for Disk I/O :""$CPUIO""%" >> ./pbg_ser$TODAY.log
 		echo "" >> ./pbg_ser${TODAY}.log
 		archive_command=`cat $DATA_DIR/postgresql.auto.conf | grep -v "#" | grep archive_command | tail -n 1`
